6d2ae2386805d4c08506c212a771d691f8ae7bdb
Your Name <anthony197542@gmail.com>
2025-12-02 11:11:41 -0500

    2 commit

--- react-course-crash/src/components/Todo.jsx
+++ react-course-crash/src/components/Todo.jsx
@@ -1,15 +1,16 @@
 import './Todo.css';
 
 function Todo({ title, paragraph}) {
-    console.log(title)
+    console.log(title);
     return (
         <div className="todo">
             <h2>{title}</h2>
             <p>{paragraph}</p>
-                    <button onClick=>{() => {
-                        console.log('clicked')
-                    }}<Delete</button>
+                    <button onClick=>{() => console.log('clicked')}>
+                    Delete
+                    </button>
                     </div>
+                   
               
             
         
